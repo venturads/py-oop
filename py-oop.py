5027ac6f5114f131731d6f4a83ba917b431dbd3d
venturads <albert@venturads.com>
2020-02-11 13:59:12 -0800

    fix method oop

--- py-oop.py
+++ py-oop.py
@@ -6,7 +6,8 @@ class User:
         self.name = name
         self.age = age
         self.hobbies = hobbies
-        print("Welcome new hire:{}, age: {}")
+        print("Welcome new hire: ", self.name, ", age: ", self.age)
+        
 kobe = User("Kobe", 41, ['basketball', 'movies'])
-
+shaq = User("Shaq", 55, ['food', 'donuts'])
 #print("Welcome new hire: {}, age: {} ".format(kobe.name, kobe.age))
