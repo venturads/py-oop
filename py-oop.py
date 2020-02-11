e7ed75279c53a6f2731d2927b8f3dd2e41689143
venturads <albert@venturads.com>
2020-02-11 10:25:45 -0800

    first class

--- py-oop.py
+++ py-oop.py
@@ -1,1 +1,10 @@
 #python object-oriented programming
+
+class User:
+
+    def __init__(self, name, age, hobbies)
+        self.name = name
+        self.age = age
+        self.hobbies
+
+kobe = User("Kobe", 41, ['basketball', 'movies'])
