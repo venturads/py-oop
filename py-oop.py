8a99391f2980f7bd6f987b459b5f1651b4def1e0
venturads <albert@venturads.com>
2020-02-11 10:30:21 -0800

    added print

--- py-oop.py
+++ py-oop.py
@@ -2,9 +2,11 @@
 
 class User:
 
-    def __init__(self, name, age, hobbies)
+    def __init__(self, name, age, hobbies):
         self.name = name
         self.age = age
-        self.hobbies
+        self.hobbies = hobbies
 
 kobe = User("Kobe", 41, ['basketball', 'movies'])
+
+print("Welcome new hire: {}, age: {} ".format(kobe.name, kobe.age))
